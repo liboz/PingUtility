dac2a4328aba9d605d798a4a533aa19d3e6546ad
Libo Zeng <ringil.sword@gmail.com>
2021-08-06 12:35:37 -0400

    update

--- PingUtility/Program.fs
+++ PingUtility/Program.fs
@@ -12,77 +12,91 @@ let googleUrl = "google.com"
 
 let mutable sw = File.AppendText(logFileName)
 
-type IPInfo = { IPAddress : IPAddress; Name : string}
+type IPInfo = { IPAddress: IPAddress; Name: string }
 
-let pingAsync (target: IPAddress) = 
+let pingAsync (target: IPAddress) =
     async {
-        let pingSender = new Ping ();
-        let targetReply = pingSender.SendPingAsync(target)
+        let pingSender = new Ping()
+        let targetReply = pingSender.SendPingAsync(target, 250)
 
         let! result = Async.AwaitTask targetReply
 
         return result
     }
 
-let pingAsyncCatch target = 
-    pingAsync target
-    |> Async.Catch
+let pingAsyncCatch target = pingAsync target |> Async.Catch
 
-let pingAyncWithName target = 
+let pingAyncWithName target =
     async {
         let! result = pingAsyncCatch target.IPAddress
 
         return target, result
     }
 
-let pingAll (targets: IPInfo[]) (router: IPInfo) = 
-    let local = {IPAddress = IPAddress.Loopback; Name = "localhost"}
-    let all = [|local; router; |] |> Array.append targets
+let pingAll (targets: IPInfo []) (router: IPInfo) =
+    let local =
+        { IPAddress = IPAddress.Loopback
+          Name = "localhost" }
+
+    let all =
+        [| local; router |] |> Array.append targets
+
     all
     |> Array.map pingAyncWithName
     |> Async.Parallel
     |> Async.RunSynchronously
 
-let pingToText info (reply: Choice<PingReply, exn>) = 
+let pingToText info (reply: Choice<PingReply, exn>) =
     match reply with
-    | Choice1Of2 pingResponse -> 
+    | Choice1Of2 pingResponse ->
         if pingResponse.Status = IPStatus.Success then
             sprintf "%s: %O ms" info.Name pingResponse.RoundtripTime
         else
             sprintf "%s: %O" info.Name pingResponse.Status
     | Choice2Of2 e -> sprintf "%s: %O" info.Name e.Message
-    
 
-let ipInfoFromUrl (url: string) = 
+
+let ipInfoFromUrl (url: string) =
     let dnsInfo = Dns.GetHostEntry(url)
     let ip = dnsInfo.AddressList.[0]
-    { IPAddress = ip; Name = url}
- 
+    { IPAddress = ip; Name = url }
 
-let pingAndLog (targetUrl: string) routerIP = 
-    let router = { IPAddress = routerIP; Name = "router"}
+
+let pingAndLog (targetUrl: string) routerIP =
+    let router =
+        { IPAddress = routerIP
+          Name = "router" }
+
     let target = ipInfoFromUrl targetUrl
     let google = ipInfoFromUrl googleUrl
 
-    let targets = [|target; google|]
+    let targets = [| target; google |]
 
     while true do
-        let result = pingAll targets router
-                     |> Array.map (fun i -> let info, reply = i
-                                            pingToText info reply)
-    
-        let timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff",CultureInfo.InvariantCulture);
+        let result =
+            pingAll targets router
+            |> Array.map
+                (fun i ->
+                    let info, reply = i
+                    pingToText info reply)
+
+        let timestamp =
+            DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture)
+
         let line = sprintf "%s: %A" timestamp result
         sw.WriteLine(line)
         Console.WriteLine(line)
         sw.Flush()
+
         if FileInfo(logFileName).Length > 5000000L then // 5 MB
             sw.Close()
             sw.Dispose()
+
             if File.Exists(logBackUpFileName) then
                 File.Delete(logBackUpFileName)
             else
                 ()
+
             File.Move(logFileName, logBackUpFileName)
             File.Delete(logFileName)
             sw <- File.AppendText(logFileName)
@@ -96,44 +110,56 @@ let ``ask for target`` () =
     let targetUrl = Console.ReadLine()
     printfn "Save target url? (Y/N)"
     let response = Console.ReadLine()
+
     if response = "Y" then
         use configFile = File.CreateText(configFileName)
         configFile.WriteLine(targetUrl)
+
     targetUrl
 
 let ``find router ip`` () =
-    let adapters  = NetworkInterface.GetAllNetworkInterfaces();
-    let routerAdapter = 
+    let adapters =
+        NetworkInterface.GetAllNetworkInterfaces()
+
+    let routerAdapter =
         adapters
         |> Array.map (fun a -> a.GetIPProperties())
-        |> Array.tryFind(fun ap -> 
+        |> Array.tryFind
+            (fun ap ->
                 let addresses = ap.DhcpServerAddresses
                 addresses.Count > 0)
+
     routerAdapter
     |> Option.map (fun ap -> ap.DhcpServerAddresses.[0])
 
-let ``ask and ping`` routerIP = 
+let ``ask and ping`` routerIP =
     let targetUrl = ``ask for target`` ()
     pingAndLog targetUrl routerIP
 
 [<EntryPoint>]
-let main argv = 
+let main argv =
     let routerIP = ``find router ip`` ()
+
     match routerIP with
-    | None -> printfn "No Internet Connection"
-              0
-    | Some r -> if File.Exists(configFileName) then
-                    let lines = File.ReadAllLines(configFileName)
-                    if lines.Length <> 1 then
-                        ``ask and ping`` r
-                    else
-                        printfn "Use stored config file url? (Y/N)"
-                        let response = Console.ReadLine()
-                        if response = "Y" then
-                            let targetUrl = lines |> Array.exactlyOne
-                            pingAndLog targetUrl r
-                        else
-                             ``ask and ping`` r
+    | None ->
+        printfn "No Internet Connection"
+        0
+    | Some r ->
+        if File.Exists(configFileName) then
+            let lines = File.ReadAllLines(configFileName)
+
+            if lines.Length <> 1 then
+                ``ask and ping`` r
+            else
+                printfn "Use stored config file url? (Y/N)"
+                let response = Console.ReadLine()
+
+                if response = "Y" then
+                    let targetUrl = lines |> Array.exactlyOne
+                    pingAndLog targetUrl r
                 else
                     ``ask and ping`` r
-                0
+        else
+            ``ask and ping`` r
+
+        0
